7042cc4db10517efe3ce18c1cef3e735de5690f3
GrigoryTem <41284129+GrigoryTem@users.noreply.github.com>
2018-07-22 19:07:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,17 +9,20 @@ import time
 Client = discord.Client() #Initialise Client 
 client = commands.Bot(command_prefix = "?") #Initialise client bot
 
+greetings = ["hey", "hi", "hello"]
+
 @client.event 
 async def on_ready():
     print("Bot is online and connected to Discord") #This will be called when the bot connects to the server
 
 @client.event
 async def on_message(message):
-    if message.content == "Hi":
-        await client.send_message(message.channel, "hello, type help")
-    if message.content == "help":
-        await client.send_message(message.channel, "mafia\nmaniac")
+    for word in greetings:
+    if message.content == word:
+        await client.send_message(message.channel, "mafia - I can choose victim\nmaniac - hm what is victim?")
     if message.content == "mafia":
-        await client.send_message(message.channel, random.choice(["I think who votes against me", "I will agree with the most players", "Anna", "Phantom"]))
+        await client.send_message(message.channel, "I dont care")
+    if "Voting" in message.content:
+        await client.send_message(message.channel, random.choice(["Akbar", "Fox", "Marcus", "Grigory"]))
 
-client.run("NDY3NzMyMTgzODQ3NjY1NjY0.DjTyFQ.gjcMyAuCvvmXj7WctLtn3ex7v1c") #Replace token with your bots token
+client.run("NDY3NzMyMTgzODQ3NjY1NjY0.DjY7qg.D6krOUieAnOs4zVzD5Fidf_YkvA") #Replace token with your bots token
